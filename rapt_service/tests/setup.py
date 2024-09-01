aed6c0cac77f6add25378d13382cb400ce7eeb51
PeterKahenya <kahenya0@gmail.com>
2024-09-01 14:21:34 +0300

    refactor: setup for tests

---
+++ rapt_service/tests/setup.py
@@ -0,0 +1,39 @@
+from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings
+import pytest
+from sqlalchemy import create_engine,text
+from sqlalchemy.orm import sessionmaker
+import models
+
+
+class Settings(BaseSettings):
+    mysql_driver: str = "mysql+pymysql"
+    test_database_host: str
+    test_database_port: int
+    test_database_user: str
+    test_database_password: str
+    test_database_name: str
+    verification_code_length: int = 6
+    verification_code_expiry_seconds: int = 300
+    jwt_secret_key: str = "iufidfufidsfudfisudfsudfiudfidufd8f9df89f8d98fdf98"
+    jwt_algorithm: str = "HS256"
+    access_token_expiry_minutes: int = 60
+
+settings = Settings()
+TEST_DATABASE_URL = f"{settings.mysql_driver}://{settings.test_database_user}:{settings.test_database_password}@{settings.test_database_host}:{settings.test_database_port}"
+
+
+@pytest.fixture(scope="session")
+def db():
+    engine = create_engine(TEST_DATABASE_URL)
+    with engine.connect() as conn:
+        conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {settings.test_database_name};"))
+    engine = create_engine(f"{TEST_DATABASE_URL}/{settings.test_database_name}")
+    models.Model.metadata.create_all(engine)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    session = SessionLocal(bind=engine)
+    yield session
+    session.rollback()
+    session.close()
+    models.Model.metadata.drop_all(bind=engine)
+    engine.dispose()
---
+++ rapt_service/tests/test_models.py
@@ -0,0 +1,181 @@
+from .setup import db, settings
+import models
+from sqlalchemy.sql import select
+import pytest
+
+pytest_plugins = ('pytest_asyncio',)
+
+# test content_type model
+def test_content_type_model(db):
+    content_type = models.ContentType(content = "users")
+    db.add(content_type)
+    db.commit()
+    assert content_type in db
+
+# test permission model
+def test_permission_model(db):
+    user_content_type = db.execute(select(models.ContentType).where(models.ContentType.content == "users")).scalars().first()
+    permission = models.Permission(name="Can View Users",codename="view_users",content_type=user_content_type)
+    db.add(permission)
+    db.commit()
+    assert permission in db
+
+# test role model
+def test_role_model(db):
+    user_content_type = db.execute(select(models.ContentType).where(models.ContentType.content == "users")).scalars().first()
+    permission1 = models.Permission(name="Can Edit Users",codename="edit_users",content_type=user_content_type)
+    permission2 = models.Permission(name="Can Delete Users",codename="delete_users",content_type=user_content_type)
+    role = models.Role(name="Admin",description="Admin Role")
+    role.permissions.append(permission1)
+    role.permissions.append(permission2)
+    db.add(role)
+    db.commit()
+    assert role in db
+    
+# test user model, roles and contacts, verification code generation and validation and jwt token generation and validation
+@pytest.mark.asyncio
+async def test_user_model(db):
+    user = models.User(name="Test User", phone="1234567890")
+    db.add(user)
+    db.commit()
+    assert user in db
+    contact1 = models.User(name="Test User 1", phone="1234567891")
+    contact2 = models.User(name="Test User 2", phone="1234567892")
+    user.contacts.append(contact1)
+    user.contacts.append(contact2)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    assert contact1 in user.contacts
+    assert contact2 in user.contacts
+    role = db.execute(select(models.Role).where(models.Role.name == "Admin")).scalar_one()
+    role2 = models.Role(name="User",description="User Role")
+    db.add(role2)
+    db.commit()
+    db.refresh(role2)
+    user.roles.append(role)
+    user.roles.append(role2)
+    db.commit()
+    db.refresh(user)
+    assert role in user.roles
+    assert role2 in user.roles
+    #test verification code generation and validation
+    user = await user.create_verification_code(db,settings.verification_code_length,settings.verification_code_expiry_seconds)
+    assert user.phone_verification_code != None
+    assert user.phone_verification_code_expiry_at != None
+    validation_status = await user.validate_verification_code(user.phone_verification_code)
+    assert validation_status == True
+    validation_status = await user.validate_verification_code("123456")
+    assert validation_status == False
+    #test jwt token generation and validation
+    token = user.create_jwt_token(secret=settings.jwt_secret_key,algorithm=settings.jwt_algorithm,expiry_minutes=settings.access_token_expiry_minutes)
+    assert user.phone == models.User.verify_jwt_token(db,token,secret=settings.jwt_secret_key,algorithm=settings.jwt_algorithm)
+    assert models.User.verify_jwt_token(db,token,secret=settings.jwt_secret_key,algorithm=settings.jwt_algorithm+"1") == None
+    assert models.User.verify_jwt_token(db,token,secret="lsdslkdsldk",algorithm=settings.jwt_algorithm) == None
+
+# test clientapp model and user relationship
+def test_clientapp_model(db):
+    user = db.execute(select(models.User).where(models.User.name == "Test User")).scalar_one()
+    clientapp = models.ClientApp(name="Test App",description="Test App Description",user_id=user.id)
+    db.add(clientapp)
+    db.commit()
+    db.refresh(clientapp)
+    db.refresh(user)
+    assert clientapp in db
+    assert clientapp.user == user
+    assert clientapp.client_id != None
+    assert clientapp.client_secret != None
+    assert clientapp in user.client_apps
+    # test client_id and client_secret generation
+    clientapp2 = models.ClientApp(name="Test App 2",description="Test App Description",user_id=user.id)
+    db.add(clientapp2)
+    db.commit()
+    db.refresh(clientapp2)
+    assert clientapp2.client_id != None
+    assert clientapp2.client_secret != None
+    assert clientapp2.client_id != clientapp.client_id
+    assert clientapp2.client_secret != clientapp.client_secret
+    
+
+# test chatroom model and user relationship
+def test_chatroom_model(db):
+    member1 = db.execute(select(models.User).where(models.User.name == "Test User")).scalar_one()
+    member2 = db.execute(select(models.User).where(models.User.name == "Test User 1")).scalar_one()
+    chatroom = models.ChatRoom(fcm_room_id="FCMID00001",socket_room_id="SOCKROOMID0001",members=[member1,member2])
+    db.add(chatroom)
+    db.commit()
+    db.refresh(chatroom)
+    db.refresh(member1)
+    db.refresh(member2)
+    assert chatroom in db
+    assert member1 in chatroom.members
+    assert member2 in chatroom.members
+    assert chatroom in member1.chatrooms
+    assert chatroom in member2.chatrooms
+    
+    # chatroom must have at least 2 members
+    with pytest.raises(ValueError):
+        models.ChatRoom(fcm_room_id="FCMID00002",socket_room_id="SOCKROOMID0002",members=[member1])
+    # chatroom must have unique members
+    with pytest.raises(ValueError):
+        models.ChatRoom(fcm_room_id="FCMID00003",socket_room_id="SOCKROOMID0003",members=[member1,member2,member1])
+    # chatroom must have fcm_room_id and socket_room_id
+    with pytest.raises(ValueError):
+        models.ChatRoom(fcm_room_id=None,socket_room_id="SOCKROOMID0002",members=[member1,member2])
+    with pytest.raises(ValueError):
+        models.ChatRoom(fcm_room_id="FCMID00002",socket_room_id=None,members=[member1,member2])
+    
+# test group model and chatroom relationship
+def test_group_model(db):
+    chatroom = db.execute(select(models.ChatRoom).where(models.ChatRoom.fcm_room_id == "FCMID00001")).scalar_one()
+    group = models.Group(name="Test Group",description="Test Group Description",chatroom_id=chatroom.id)
+    db.add(group)
+    db.commit()
+    db.refresh(group)
+    db.refresh(chatroom)
+    assert group in db
+    assert group.chatroom == chatroom
+    assert chatroom.group == group
+    
+# test chat model and chatroom relationship and sender relationship
+def test_chat_model(db):
+    chatroom = db.execute(select(models.ChatRoom).where(models.ChatRoom.fcm_room_id == "FCMID00001")).scalar_one()
+    sender = db.execute(select(models.User).where(models.User.name == "Test User")).scalar_one()
+    chat = models.Chat(message="Test Chat",room=chatroom,sender=sender)
+    db.add(chat)
+    db.commit()
+    db.refresh(chat)
+    db.refresh(chatroom)
+    db.refresh(sender)
+    assert chat in db
+    assert chat.room == chatroom
+    assert chat.sender == sender
+    assert chat.message == "Test Chat"
+    assert chat in chatroom.room_chats
+    assert chat in sender.chats
+    with pytest.raises(ValueError):
+        models.Chat(message=None,room=chatroom,sender=sender)
+    with pytest.raises(ValueError):
+        models.Chat(message="Test Chat",room=chatroom,sender=None)
+    with pytest.raises(ValueError):
+        models.Chat(message="Test Chat",room=None,sender=sender)
+    with pytest.raises(ValueError):
+        models.Chat(message="Test Chat",room=None,sender=None)
+    #test sender must be a member of the chatroom
+    sender2 = db.execute(select(models.User).where(models.User.name == "Test User 2")).scalar_one()
+    with pytest.raises(ValueError):
+        models.Chat(message="Test Chat",room=chatroom,sender=sender2)
+
+# test media model and chat relationship
+def test_media_model(db):
+    chat = db.execute(select(models.Chat).where(models.Chat.message == "Test Chat")).scalar_one()
+    media = models.Media(link="http://test.com/test.jpg",file_type="jpg",chat_id=chat.id)
+    db.add(media)
+    db.commit()
+    db.refresh(media)
+    db.refresh(chat)
+    assert media in db
+    assert media.chat == chat
+    assert media in chat.media
+
+
