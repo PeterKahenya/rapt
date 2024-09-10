bc2dd37849c92de833a742fd6257fc2c197e3d34
PeterKahenya <kahenya0@gmail.com>
2024-09-10 15:29:34 +0300

    Refactor utils.py to improve random string generation and add SMS sending functionality

--- rapt_service/utils.py
+++ rapt_service/utils.py
@@ -1,12 +1,41 @@
 import uuid
 import random
+import base64
+import json
+import requests
+from config import settings
+import secrets
+import string
 
 
 def generate_random_string(length:int=6):
-    return ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', k=length))
+    choice_characters = string.ascii_uppercase+ string.digits
+    random_string = ''.join(secrets.choice(choice_characters) for _ in range(length))
+    return random_string
 
 def generate_client_id():
     return str(uuid.uuid4())
 
 def generate_client_secret():
-    return str(uuid.uuid4())+str(uuid.uuid4())
+    return secrets.token_urlsafe(50)
+
+async def smsleopard_send_sms(phone: str, message: str) -> bool:
+    try:
+        phone = phone.strip()
+        url = f"{settings.smsleopard_base_url}/sms/send"
+        credentials = f"{settings.smsleopard_api_key}:{settings.smsleopard_api_secret}"
+        headers = {
+            "Authorization": f"Basic {base64.b64encode(credentials.encode()).decode()}"
+        }
+        body = {
+            "source": "smsleopard",
+            "destination": [{"number": phone}],
+            "message": message
+        }
+        response = requests.post(url, data=json.dumps(body), headers=headers)
+        if response.status_code == 201 and response.json().get("success"):
+            return True
+        else:
+            raise Exception(f"{response.status_code} - {response.text}")
+    except Exception as e:
+        raise e
