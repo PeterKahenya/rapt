5aac617a4e39c473f1a1a95a50bebe9bd46719c4
PeterKahenya <kahenya0@gmail.com>
2024-10-04 18:17:03 +0300

    fix socket endpoint handler call

--- rapt_service/api.py
+++ rapt_service/api.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI, Request, WebSocket
+from fastapi import Depends, FastAPI, Request, WebSocket
+from pydantic import UUID4
 import uvicorn
 import os
 import auth
 import chat
+from depends import authorize, get_db
 import sockets
 from honeybadger import honeybadger, contrib
 from fastapi.routing import APIRouter
 from honeybadger.contrib.fastapi import HoneybadgerRoute
 from config import settings
+from sqlalchemy.orm import Session
+import models
 
 fastapi_config = {
     "title":"RaptChat Service",
@@ -20,9 +24,14 @@ app = FastAPI(**fastapi_config)
 app.include_router(auth.router,prefix="/auth")
 app.include_router(chat.router,prefix="/chat")
 
-async def chatsocket_wrapper(websocket: WebSocket, room_id: str):
+async def chatsocket_wrapper(
+                        websocket: WebSocket, 
+                        room_id: UUID4,
+                        user: models.User = Depends(authorize(perm="create_chats")),
+                        db: models.Session = Depends(get_db)
+                    ):
     try:
-        await sockets.chatsocket(websocket, room_id)
+        await sockets.chatsocket(websocket, room_id, user=user, db=db)
     except Exception as e:
         honeybadger.notify(e)
         raise
