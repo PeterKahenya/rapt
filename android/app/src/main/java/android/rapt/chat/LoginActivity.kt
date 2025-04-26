9ab3b820e0ff82db2517d98e3a836b4401b1d1e7
PeterKahenya <kahenya0@gmail.com>
2025-04-27 01:45:18 +0300

    update reference to theme

--- android/app/src/main/java/android/rapt/chat/LoginActivity.kt
+++ android/app/src/main/java/android/rapt/chat/LoginActivity.kt
@@ -1,6 +1,7 @@
 package android.rapt.chat
 
 import android.os.Bundle
+import android.rapt.chat.theme.RaptTheme
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import android.rapt.chat.ui.theme.RaptTheme
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
