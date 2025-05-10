77e57856a5a16be342e6e3c2e34637a575e64167
PeterKahenya <kahenya0@gmail.com>
2025-05-10 23:57:16 +0300

    render chat screen with it's viewmodel

--- android/app/src/main/java/android/rapt/chat/ChatActivity.kt
+++ android/app/src/main/java/android/rapt/chat/ChatActivity.kt
@@ -1,20 +1,33 @@
 package android.rapt.chat
 
 import android.os.Bundle
+import android.rapt.chat.screens.ChatScreen
 import android.rapt.chat.theme.RaptTheme
+import android.rapt.chat.viewmodels.ChatViewModel
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ChatActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         val contactId = intent.getStringExtra("contactId")
+        val roomId = intent.getStringExtra("roomId")
+        Log.i("ChatActivity onCreate","contactId: $contactId roomId: ${roomId?: "null"}")
         setContent {
             RaptTheme {
-                Text("Chat Activity with Contact ID: $contactId")
+                val viewModel = hiltViewModel<ChatViewModel>()
+                if (contactId != null) {
+                    viewModel.setupChat(contactId, roomId)
+                    ChatScreen(viewModel, contactId)
+                }else{
+                    Text(text = "Invalid Contact ID")
+                }
             }
         }
     }
