514bafc8a3ff3a2a1bc5603e50a8120edcb19156
PeterKahenya <kahenya0@gmail.com>
2025-05-11 22:46:09 +0300

    implement chats list repo

--- android/app/src/main/java/android/rapt/chat/repositories/ChatsRepository.kt
+++ android/app/src/main/java/android/rapt/chat/repositories/ChatsRepository.kt
@@ -1,71 +1,95 @@
 package android.rapt.chat.repositories
 
-import android.rapt.chat.models.ContactUpload
+import android.rapt.chat.models.ChatRoomCreate
+import android.rapt.chat.models.MemberObj
+import android.rapt.chat.sources.ChatRoomDao
 import android.rapt.chat.sources.ContactDao
+import android.rapt.chat.sources.DBChat
+import android.rapt.chat.sources.DBChatRoom
+import android.rapt.chat.sources.DBChatRoomMember
+import android.rapt.chat.sources.DBChatRoomWithMembersAndMessages
 import android.rapt.chat.sources.DBContact
 import android.rapt.chat.sources.RaptAPI
-import android.rapt.chat.sources.RaptContentProvider
-import android.util.Log
 import javax.inject.Inject
 
-interface ContactsRepository {
-    suspend fun sync(): List<DBContact>
-    suspend fun searchContacts(query: String): List<DBContact>
-    suspend fun deleteContact(contact: DBContact)
+interface ChatsRepository {
+    suspend fun sync(): List<DBChatRoomWithMembersAndMessages>
 }
 
-class ContactsRepositoryImpl @Inject constructor(
+class ChatsRepositoryImpl @Inject constructor(
     private val api: RaptAPI,
+    private val chatRoomDB: ChatRoomDao,
     private val contactDB: ContactDao,
-    private val contentProvider: RaptContentProvider,
     private val authRepository: AuthRepository
-) : ContactsRepository {
-    override suspend fun sync(): List<DBContact> {
+) : ChatsRepository {
+
+    override suspend fun sync(): List<DBChatRoomWithMembersAndMessages> {
         try {
             val auth = authRepository.auth() ?: throw Exception("No auth found")
-            // upload contacts to api that are in phone but not in api
-            val phoneContacts = contentProvider.getContacts()
-            var apiContacts = api.getContacts(accessToken = "Bearer ${auth.accessToken}", userId = auth.userId)
-            val apiUploadList = mutableListOf<ContactUpload>()
-            for (phoneContact in phoneContacts){
-                if (phoneContact.phone !in apiContacts.map { it.phone }){
-                    apiUploadList.add(ContactUpload(name = phoneContact.name, phone = phoneContact.phone))
-                }
-            }
-            api.addContacts(
-                accessToken = "Bearer ${auth.accessToken}",
-                userId = auth.userId,
-                addContactsRequest = apiUploadList
-            )
-            // any contact not in the db should be added
-            val dbContacts = contactDB.getAll()
-            apiContacts = api.getContacts(accessToken = "Bearer ${auth.accessToken}", userId = auth.userId)
-            for (apiContact in apiContacts) {
-                if (apiContact.phone !in dbContacts.map { it.phone }) {
-                    contactDB.insert(
-                        DBContact(
-                            name = apiContact.name,
-                            phone = apiContact.phone,
-                            contactId = apiContact.contactId,
-                            userId = auth.userId,
-                            isActive = apiContact.isActive
+            val dbChatRooms = chatRoomDB.getAllChatRooms()
+            val apiChatRooms = api.getChatRooms(accessToken = "Bearer ${auth.accessToken}")
+            // Any chatroom in the db not in the api should be created in the api
+            for (dbChatRoom in dbChatRooms) {
+                if (dbChatRoom.chatRoomId !in apiChatRooms.map { it.id }) {
+                    api.createChatRoom(
+                        accessToken = "Bearer ${auth.accessToken}",
+                        chatRoomRequest = ChatRoomCreate(
+                            members = chatRoomDB.getChatRoomMembers(dbChatRoom.chatRoomId).map { MemberObj(id = it, name = "", phone = "") }
                         )
                     )
                 }
             }
-            return contactDB.getAll()
+            // Any chatroom in the api not in the db should be added
+            for (apiChatRoom in apiChatRooms) {
+                if (apiChatRoom.id !in dbChatRooms.map { it.chatRoomId }) {
+                    chatRoomDB.insertChatRoom(DBChatRoom(chatRoomId = apiChatRoom.id))
+                }
+                for (member in apiChatRoom.members) {
+                    if (contactDB.getByContactId(contactId = member.id) == null){
+                        contactDB.insert(
+                            DBContact(
+                                name = member.name,
+                                phone = member.phone,
+                                userId = auth.userId,
+                                contactId = member.id,
+                                isActive = false
+                            )
+                        )
+                        chatRoomDB.insertChatRoomMember(
+                            chatRoomMember = DBChatRoomMember(
+                                contactId = member.id,
+                                chatRoomId = apiChatRoom.id
+                            )
+                        )
+                    }
+                }
+                for (message in apiChatRoom.chats) {
+                    if (chatRoomDB.getMessageById(message.id) == null) {
+                        chatRoomDB.insertMessage(
+                            DBChat(
+                                chatId = message.id,
+                                senderId = message.sender.id,
+                                chatRoomId = apiChatRoom.id,
+                                message = message.message,
+                                isRead = message.isRead,
+                                timestamp = System.currentTimeMillis()
+                            )
+                        )
+                    }
+                }
+            }
+            val chatRooms = chatRoomDB.getChatRoomsWithMembersAndMessages().map {
+                DBChatRoomWithMembersAndMessages(
+                    room = it.room,
+                    chats = it.chats,
+                    members = it.members.filter { contact -> contact.phone != auth.phone }
+                )
+            }
+            return chatRooms
+
         } catch (ex: Exception){
-            return contactDB.getAll()
+            return chatRoomDB.getChatRoomsWithMembersAndMessages()
         }
-    }
-
-    override suspend fun searchContacts(query: String): List<DBContact> {
-        return contactDB.search(query)
-    }
-
-    override suspend fun deleteContact(contact: DBContact) {
-        // TODO: delete contact from api as well
-        contactDB.delete(contact)
     }
 
 }
